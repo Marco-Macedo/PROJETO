67e6e1e618f2ed1c57c076e955c9d0c5d36b635f
Marco-Macedo <marcomacedo@ipvc.pt>
2020-10-27 13:02:43 +0000

    Create the repository

---
+++ app/src/main/java/com/example/projeto/db/TitleRepository.kt
@@ -0,0 +1,16 @@
+package com.example.projeto.db
+
+import androidx.lifecycle.LiveData
+import com.example.projeto.dao.TitleDao
+import com.example.projeto.entities.Title
+
+class TitleRepository(private val titleDao: TitleDao) {
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allTitles: LiveData<List<Title>> = titleDao.getAlphabetizedTitles()
+
+    suspend fun insert(title: Title) {
+        titleDao.insert(title)
+    }
+}
