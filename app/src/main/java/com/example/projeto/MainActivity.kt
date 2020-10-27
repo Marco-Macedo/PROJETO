7c6777daedfc8caaba12c9342cfd759743d4e26d
Marco-Macedo <marcomacedo@ipvc.pt>
2020-10-27 15:47:26 +0000

    Add newWordAcitivity

--- app/src/main/java/com/example/projeto/MainActivity.kt
+++ app/src/main/java/com/example/projeto/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.projeto
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,10 +8,13 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projeto.adapters.TitleAdapter
+import com.example.projeto.entities.Title
 import com.example.projeto.viewModel.TitleViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -27,19 +31,54 @@ class MainActivity : AppCompatActivity() {
         val adapter = TitleAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // view model
+        titleViewModel = ViewModelProvider(this).get(TitleViewModel::class.java)
+        titleViewModel.allTitles.observe(this, { titles ->
+            // Update the cached copy of the words in the adapter.
+            titles?.let { adapter.setTitles(it) }
+        })
+
+        //Fab
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent( this@MainActivity, AddTitle::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(AddTitle.EXTRA_REPLY)?.let {
+                val title = Title(title = it, notes = "Gym", date = "27/10/2020")
+                titleViewModel.insert(title)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Titulo vazio: não inserido",
+                Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+
+
+
+
+
+
+
+
 
     /*fun inserir(view: View){
         val intent = Intent(this, Inserir::class.java).apply {
         }
         startActivity(intent)
     }*/
-
-
-
-
-
-
 
 
 
