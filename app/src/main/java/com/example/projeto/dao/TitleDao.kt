41703f8c4c9b195f3e1a92829f5952f2892a40a8
Marco-Macedo <marcomacedo@ipvc.pt>
2020-10-27 12:33:33 +0000

    update

--- app/src/main/java/com/example/projeto/dao/TitleDao.kt
+++ app/src/main/java/com/example/projeto/dao/TitleDao.kt
@@ -7,12 +7,13 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.projeto.entities.Title
 
+@Dao
 interface TitleDao {
     @Query("SELECT * from title_table ORDER BY title ASC")
     fun getAlphabetizedTitles(): LiveData<List<Title>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(word: Title)
+    suspend fun insert(title: Title)
 
     @Query("DELETE FROM title_table")
     suspend fun deleteAll()
